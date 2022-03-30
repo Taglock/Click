e6355cb829a9b9d6b56bd72c7612dd98cbddaafb
Taglock <39584594+Taglock@users.noreply.github.com>
2022-03-30 11:34:01 +0500

    initial commit

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1648621176,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/hp_bar.png",
+		"images/sprite2-sheet0.png",
+		"images/tilemap.png",
+		"images/background.png",
+		"images/9patch.png",
+		"images/sprite3-sheet0.png",
+		"media/bass-bochka-v-bolshom-prostranstve.ogg",
+		"media/brosok-odnoy-monetki-v-obschuyu-kuchu.ogg",
+		"media/tarelka-30.ogg",
+		"media/tarelka-39.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
